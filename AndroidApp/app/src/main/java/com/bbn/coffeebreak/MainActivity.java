27e9e2d6eabe3fe2d1177e75307c8dd4deda2ff7
zratliff <zratliff@bbn.com>
2019-10-08 12:21:35 -0400

    Refactoring notifications

--- AndroidApp/app/src/main/java/com/bbn/coffeebreak/MainActivity.java
+++ AndroidApp/app/src/main/java/com/bbn/coffeebreak/MainActivity.java
@@ -249,31 +249,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        if(getIntent().getStringExtra("meetingID") != null && getIntent().getStringExtra("address") != null){
-            Log.d(TAG, "Got notif intent!");
-            Intent showMeetingLocation = new Intent();
-            showMeetingLocation.putExtra("address", getIntent().getStringExtra("address"));
-            showMeetingLocation.putExtra("latitude", getIntent().getFloatExtra("latitude", 0.0f));
-            showMeetingLocation.putExtra("longitude", getIntent().getFloatExtra("longitude", 0.0f));
-            showMeetingLocation.setAction(getString(R.string.broadcast_show_meeting_location));
-            mLocalBroadcastManager.sendBroadcast(showMeetingLocation);
-        } else if (getIntent().getStringExtra("meetingID") != null && getIntent().getStringExtra("organizer") != null) {
-            Intent showMeetingRequest = new Intent();
-            Log.d(TAG, "Got intent with meetingID: " + getIntent().getStringExtra("meetingID"));
-            showMeetingRequest.putExtra("meetingID", getIntent().getStringExtra("meetingID"));
-            showMeetingRequest.putExtra("organizer", getIntent().getStringExtra("organizer"));
-            showMeetingRequest.putStringArrayListExtra("attendees", getIntent().getStringArrayListExtra("attendees"));
-            showMeetingRequest.putExtra("begin", getIntent().getStringExtra("begin"));
-            showMeetingRequest.putExtra("end", getIntent().getStringExtra("end"));
-            showMeetingRequest.setAction(getString(R.string.broadcast_show_meeting_request));
-            mLocalBroadcastManager.sendBroadcast(showMeetingRequest);
-        }else if(getIntent().getStringExtra("meetingID") != null){
-            Intent showMeetingCancel = new Intent();
-            Log.d(TAG, "Got intent with meetingID: " + getIntent().getStringExtra("meetingID"));
-            showMeetingCancel.putExtra("meetingID", getIntent().getStringExtra("meetingID"));
-            showMeetingCancel.putExtra("invitee", getIntent().getStringExtra("invitee"));
-            showMeetingCancel.setAction(getString(R.string.broadcast_show_meeting_cancel));
-            mLocalBroadcastManager.sendBroadcast(showMeetingCancel);
+        if(getIntent().getStringExtra("meetingID") != null){
+            mLocalBroadcastManager.sendBroadcast(getIntent());
         }
 
     }
