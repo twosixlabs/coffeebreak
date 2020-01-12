12aaa5f16b3bb6cd2399b0294d68c52f406326d6
Quinn Grier <quinn@stealthsoftwareinc.com>
2020-01-11 18:24:34 -0800

    Update the assets

--- AndroidApp/app/src/main/assets/ThreadMPC/coffeeshop_n_2_lookahead_1.cbl
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:eaab762e10c1c3a5d2d05211993425006bb1a352808da93dc1aed3a53e410d8f
-size 34213
--- AndroidApp/app/src/main/assets/ThreadMPC/coffeeshop_n_2_lookahead_3.cbl
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:86bd3476a6d547174da5cf7a2c72dd4e07c566c29c43fc73adffb2fa3d972270
-size 53976
